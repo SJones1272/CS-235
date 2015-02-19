2a1f66d0abe34cdf36c872a8ad8dcc6e813bf3de
Stephen <SJones1272@users.noreply.github.com>
2015-02-19 12:45:12 -0500

    Update to motor misc

--- Misc/motorMisc/motorMisc.ino
+++ Misc/motorMisc/motorMisc.ino
@@ -2,17 +2,19 @@ int cycle;
 
 void setup(){
   pinMode(9, OUTPUT);
-  pinMode(10, INPUT);
+  pinMode(A0, INPUT);
   cycle = 50;
   Serial.begin(9600);
 }
 
-void loop(){
-  //cycle = analogRead(10);
-  Serial.println(cycle);
-  cycle = map(analogRead(10), 0,1023 - 289, 0, 50);
+void loop() {
+  //cycle = analogRead(A0);
+  //Serial.println(cycle);
+  Serial.println(analogRead(A0));
+  cycle = map(analogRead(A0), 0,1023, 0, 256);
   digitalWrite(9, HIGH);
   delay(cycle);
   digitalWrite(9, LOW);
   delay(cycle);
-}​
+
+}
