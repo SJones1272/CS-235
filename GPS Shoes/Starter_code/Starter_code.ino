f21be1a11bf73e86d838596e8391da06c5960d16
AlanYoung4Life <alan.young@vikings.berry.edu>
2015-03-16 09:22:03 -0400

    Added some GPS stuff

--- GPS Shoes/Starter_code/Starter_code.ino
+++ GPS Shoes/Starter_code/Starter_code.ino
@@ -1,26 +1,61 @@
+#include <TinyGPS++.h>
 #include <Adafruit_NeoPixel.h>
+#include <SoftwareSerial.h>
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      25
 
 #define NEOPIN 6 //The control pin for the neopixels
 
+// Choose two Arduino pins to use for software serial
+// The GPS Shield uses D2 and D3 by default when in DLINE mode
+int RXPin = 2;
+int TXPin = 3;
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
+// The Skytaq EM-506 GPS module included in the GPS Shield Kit
+// uses 4800 baud by default
+int GPSBaud = 4800;
+
+// Create a TinyGPS++ object called "gps"
+TinyGPSPlus gps;
+
+SoftwareSerial gpsSerial(RXPin, TXPin);
+
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, NEOPIN, NEO_GRB + NEO_KHZ800);
 
-float dest;
+double destLat;
+double destLng;
+
 
 void setup()
 {
-  
+   // Start the Arduino hardware serial port at 9600 baud
+  Serial.begin(9600);
+
+  // Start the software serial port at the GPS's default baud
+  gpsSerial.begin(GPSBaud);
+
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println(); 
 }
 
 void loop()
 {
-  
+    // This sketch displays information every time a new sentence is correctly encoded.
+  while (gpsSerial.available() > 0)
+    if (gps.encode(gpsSerial.read())){
+      displayInfo();
+    }
+  // If 5000 milliseconds pass and there are no characters coming in
+  // over the software serial port, show a "No GPS detected" error
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected"));
+    while(true);
+  }
 }
 
 //h is the current heading
@@ -203,7 +238,7 @@ int getDirection(int h)
 
 /**
 returns a numerical destination
-0 -> N
+0 or 8 -> N
 1 -> NE
 2 -> E
 3 -> SE
@@ -212,7 +247,75 @@ returns a numerical destination
 6 -> W
 7 -> NW
 */
+int getHeading()
+{
+ double temp = gps.course.deg();
+  return round(temp/ 45);
+}
+
 int headingToDest()
 {
-  return 0;
+ double heading = gps.courseTo(
+    gps.location.lat(),
+    gps.location.lng(),
+    destLat,
+    destLng);
+   return round(heading/45); 
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+    
+    Serial.print(" Current Heading: ");
+    Serial.print(getHeading());
+    Serial.print(" Heading to dest: ");
+    Serial.print(headingToDest());
+    
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
 }
